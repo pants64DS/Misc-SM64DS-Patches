bd2191e60f3e849efefc8cdd3b39893d751bbda9
pants64DS <pants64DS@users.noreply.github.com>
2023-11-01 01:03:30 +0200

    Add an implementation for memmove

--- source/glibc.cpp
+++ source/glibc.cpp
@@ -1,7 +1,29 @@
 #include "include/SM64DS_Common.h"
 
+static void CopyBackwards(char* dest, const char* src, size_t size)
+{
+	dest += size;
+	src += size;
+
+	while (size--) *--dest = *--src;
+}
+
 extern "C"
 {
+	[[gnu::section(".memmove")]]
+	void* memmove(void* dest, const void* src, size_t size)
+	{
+		char* d = static_cast<char*>(dest);
+		const char* s = static_cast<const char*>(src);
+
+		if (d < s || d >= s + size)
+			CpuCopy8(src, dest, size);
+		else if (d > s)
+			CopyBackwards(d, s, size);
+
+		return dest;
+	}
+
 	[[gnu::section(".memcpy")]]
 	void* memcpy(void* dest, const void* src, size_t size)
 	{
